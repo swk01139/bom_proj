248e1c851faab9461495a53fb42f7fe603a535f4
lhj <swk01139@gmail.com>
2017-06-29 16:23:56 +0900

    콘텐트
    변경x

---
+++ page/content.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css" />
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="http://kenwheeler.github.io/slick/slick/slick.min.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.single-item').slick();
+        });
+
+    </script>
+
+</head>
+
+<body>
+
+    <div class="single-item">
+        <div><img src="img1.jpg" border="0"></div>
+        <div><img src="img1.jpg" border="0"></div>
+        <div><img src="img1.jpg" border="0"></div>
+    </div>
+
+</body>
+
+</html>
